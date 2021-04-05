176f4f742adeeeb3945f930975a4364cdb341ba8
Muhammad Hendra Pranajaya <mhpranajaya@gmail.com>
2021-04-05 22:34:32 +0700

    Initial Commit

---
+++ TheVeganMeals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheVeganMeals
+//
+//  Created by Muhammad Hendra Pranajaya on 05/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheVeganMeals/TheVeganMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheVeganMealsApp.swift
+//  TheVeganMeals
+//
+//  Created by Muhammad Hendra Pranajaya on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TheVeganMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
